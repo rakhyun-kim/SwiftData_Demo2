fd81c15650219f8020d01c4a6e505b9e3558a37d
RAKHYUN <kyle2598@gmail.com>
2023-11-21 16:09:07 -0600

    Initial Commit

---
+++ SwiftData_Demo2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftData_Demo2
+//
+//  Created by Rakhyun Kim on 11/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftData_Demo2/SwiftData_Demo2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftData_Demo2App.swift
+//  SwiftData_Demo2
+//
+//  Created by Rakhyun Kim on 11/21/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftData_Demo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
